872ddbe44803c737d801341cb672b5914ed083b8
Reis567 <matheusdosreis9@gmail.com>
2023-06-09 08:30:24 -0300

    Site setup Foreign key

--- djangoapp/site_setup/models.py
+++ djangoapp/site_setup/models.py
@@ -7,8 +7,13 @@ class MenuLink(models.Model):
         verbose_name_plural = 'Menu Links'
 
     text = models.CharField(max_length=50)
-    url_or_path = models.CharField(max_length=1048)
+    url_or_path = models.CharField(max_length=2048)
     new_tab = models.BooleanField(default=False)
+    site_setup = models.ForeignKey(
+        'SiteSetup', on_delete=models.CASCADE, blank=True, null=True,
+        default=None,
+    )
+    
 
     def __str__(self):
         return self.text
@@ -29,4 +34,4 @@ class SiteSetup(models.Model):
     show_footer = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.text
+        return self.title
